6fa1a1e87c36b744ae8d852c9eb9e16a4844a7c8
tianyanqiu <745916729@qq.com>
2020-01-19 16:59:43 +0800

    chore(switch): 完善单元测试

---
+++ src/Switch/__tests__/Switch.test.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from '@sinoui/theme';
+import '@testing-library/jest-dom/extend-expect';
+import Switch from '../Switch';
+
+describe('Switch 镜像测试', () => {
+  it('渲染Switch', () => {
+    const tree = renderer
+      .create(
+        <ThemeProvider theme={defaultTheme}>
+          <Switch value="switch1" color="primary" />
+        </ThemeProvider>,
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('不可用状态', () => {
+    const tree = renderer
+      .create(
+        <ThemeProvider theme={defaultTheme}>
+          <Switch value="switch2" disabled />
+        </ThemeProvider>,
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('选中状态', () => {
+    const tree = renderer
+      .create(
+        <ThemeProvider theme={defaultTheme}>
+          <Switch value="switch3" checked color="accent" />
+        </ThemeProvider>,
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
---
+++ src/Switch/__tests__/SwitchLabel.test.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components';
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from '@sinoui/theme';
+import SwitchLabel from '../SwitchLabel';
+
+describe('SwitchLabel 镜像测试', () => {
+  it('渲染SwitchLabel', () => {
+    const tree = renderer
+      .create(
+        <ThemeProvider theme={defaultTheme}>
+          <SwitchLabel />
+        </ThemeProvider>,
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('不可用状态', () => {
+    const tree = renderer
+      .create(
+        <ThemeProvider theme={defaultTheme}>
+          <SwitchLabel disabled />
+        </ThemeProvider>,
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('选中状态', () => {
+    const tree = renderer
+      .create(
+        <ThemeProvider theme={defaultTheme}>
+          <SwitchLabel checked color="accent" />
+        </ThemeProvider>,
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('验收测试', () => {
+  afterEach(cleanup);
+
+  it('focused下的样式类', () => {
+    const { container, getByTestId } = render(
+      <ThemeProvider theme={defaultTheme}>
+        <SwitchLabel checked />
+      </ThemeProvider>,
+    );
+
+    const input = container.querySelector('input') as HTMLInputElement;
+
+    fireEvent.focus(input);
+
+    expect(getByTestId('switchLabel')).toHaveClass('sinoui-switch--focused');
+
+    fireEvent.blur(input);
+    expect(getByTestId('switchLabel')).not.toHaveClass(
+      'sinoui-switch--focused',
+    );
+  });
+});
