791433b549c79e2da6de6d313b4c1b4b0e4c17ae
tianyanqiu <745916729@qq.com>
2020-02-24 11:01:28 +0800

    chore(utils):完善单元测试

---
+++ src/utils/__tests__/domHelpers.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { getScrollTop, getScrollLeft } from '../domHelpers';
+
+it('获取滚动距离', () => {
+  const { container } = render(
+    <div
+      className="parent"
+      style={{ height: 100, width: 100, overflow: 'visible' }}
+    >
+      <div
+        className="child"
+        style={{ height: 200, width: 200, overflow: 'visible', margin: 16 }}
+      >
+        这是一段测试文本这是一段测试文本这是一段测试文本这是一段测试文本这是一段测试文本这是一段测试文本这是一段测试文本这是一段测试文本这是一段测试文本这是一段测试文本这是一段测试文本这是一段测试文本这是一段测试文本这是一段测试文本这是一段测试文本这是一段测试文本这是一段测试文本这是一段测试文本这是一段测试文本这是一段测试文本这是一段测试文本这是一段测试文本这是一段测试文本
+      </div>
+    </div>,
+  );
+
+  const parent = container.querySelector('.parent') as HTMLElement;
+  const child = container.querySelector('.child') as HTMLElement;
+  const scrollTop = getScrollTop(parent, child);
+
+  expect(scrollTop).toBe(0);
+
+  const scrollLeft = getScrollLeft(parent, child);
+  expect(scrollLeft).toBe(0);
+});
