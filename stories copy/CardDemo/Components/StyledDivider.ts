78a5cc0ba9f754a92d70f873200a6d78ffc81e59
maitian0222 <361711931@qq.com>
2020-03-13 11:10:25 +0800

    chore: 删掉无用文件夹

--- stories copy/CardDemo/Components/StyledDivider.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const StyledDivider = styled.div`
-  border-top: 1px solid ${({ theme }) => theme.palette.divider};
-  margin: 16px -16px;
-`;
-
-export default StyledDivider;
--- stories copy/CardDemo/constant.ts
+++
@@ -1,6 +0,0 @@
-const mediaImgUrl =
-  'https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/2.jpg';
-
-const mediaAvatar =
-  'https://material-ui.com/static/images/cards/live-from-space.jpg';
-export { mediaImgUrl, mediaAvatar };
