d833f9a09f1388ad54fb55980263d833121c8c67
duanyaqin <361711931@qqq.com>
2020-03-20 18:03:53 +0800

    chore: 文档中增加示例

--- stories/Progress.stories.tsx
+++ stories/Progress.stories.tsx
@@ -10,5 +10,11 @@ export const 线性定量指示器 = () => <LinearDeterminate linear determinate
 export const 线性定量缓冲指示器 = () => (
   <LinearDeterminate linear determinate buffer />
 );
+export const 线条宽度为8的线性指示器 = () => (
+  <LinearDeterminate linear thickness={8} />
+);
 export const 圆形不定量指示器 = () => <LinearDeterminate />;
 export const 圆形定量指示器 = () => <LinearDeterminate determinate />;
+export const 线条宽度为8大小为80的环形指示器 = () => (
+  <LinearDeterminate thickness={8} size={80} />
+);
--- stories/ProgressDemo/LinearDeterminate.tsx
+++ stories/ProgressDemo/LinearDeterminate.tsx
@@ -1,51 +1,75 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import { defaultTheme } from '@sinoui/theme';
 import Progress from '@sinoui/core/Progress';
+import Button from '@sinoui/core/Button';
 
 const Wrapper = styled.div`
+  display: flex;
   padding-top: 10px;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
 `;
 export default function Demo({
   linear,
   determinate,
   buffer,
+  size,
+  thickness,
 }: {
   linear?: boolean;
   determinate?: boolean;
   buffer?: boolean;
+  size?: number;
+  thickness?: number;
 }) {
   const [progressValue, setProgressValue] = useState(0);
-
+  const [loading, setLoading] = useState(!determinate);
   const max = 30;
   const min = 10;
   const step = Math.floor(Math.random() * (max - min + 1) + min);
+  const timer = useRef<any>();
+  const onClick = () => {
+    if (determinate && !loading) {
+      setProgressValue(0);
+      setLoading(true);
+      timer.current = setInterval(() => {
+        setProgressValue((prev: number) => {
+          if (prev + step < 100) {
+            return prev + step;
+          }
+          const timeOut = setTimeout(() => {
+            setLoading(false);
+            clearInterval(timer.current);
+            clearTimeout(timeOut);
+          }, 500);
 
-  useEffect(() => {
-    let timer: any;
-    if (determinate) {
-      timer = setInterval(() => {
-        if (progressValue + step <= 100) {
-          setProgressValue(progressValue + step);
-        } else {
-          setProgressValue(100);
-          clearInterval(timer as any);
-        }
+          return 100;
+        });
       }, 1000);
     }
+  };
 
-    return () => clearInterval(timer as any);
-  });
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Wrapper>
-        <Progress
-          linear={linear}
-          value={progressValue}
-          determinate={determinate}
-          buffer={buffer}
-        />
-      </Wrapper>
+      {determinate && (
+        <Button raised onClick={onClick} disabled={loading}>
+          点击加载
+        </Button>
+      )}
+      {loading && (
+        <Wrapper>
+          <Progress
+            linear={linear}
+            value={progressValue}
+            determinate={determinate}
+            buffer={buffer}
+            thickness={thickness}
+            size={size}
+          />
+        </Wrapper>
+      )}
     </ThemeProvider>
   );
 }
