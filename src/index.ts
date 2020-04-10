f0412e00be0bf712f34daa1c8019f002016391d4
tianyanqiu <745916729@qq.com>
2020-04-10 17:12:28 +0800

    chore(SvgIcon): 调整并导出属性声明

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import BaseButton from './BaseButton';
 import Button from './Button';
 import IconButton from './IconButton';
 import SvgIcon from './SvgIcon';
+import type { SvgIconProps } from './SvgIcon';
 import type { Props as ButtonProps } from './Button';
 
-export type { ButtonProps };
+export type { ButtonProps, SvgIconProps };
 export { BaseButton, Button, IconButton, SvgIcon };
