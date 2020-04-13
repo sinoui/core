830cb0dbf2faabf476da9daf485c365b4c6a6a91
tianyanqiu <745916729@qq.com>
2020-04-13 17:45:29 +0800

    chore(Select): 添加属性注释

--- src/Select/Select.tsx
+++ src/Select/Select.tsx
@@ -28,33 +28,101 @@ const StyledTextInput = styled(TextInput)`
 `;
 
 export interface Props {
+  /**
+   * 宽度自适应
+   */
   autoWidth?: boolean;
-  children: React.ReactChild[];
+  /**
+   * 子元素
+   */
+  children: React.ReactNode;
+  /**
+   * 多选
+   */
   multiple?: boolean;
   displayEmpty?: boolean;
+  /**
+   * 可输入区域元素
+   */
   input?: React.ReactElement;
-  inputProps?: any;
+  /**
+   * 可输入组件的相关属性
+   */
+  inputProps?: object;
+  /**
+   * 标签内容
+   */
   label?: string;
-  labelWidth?: number;
+  /**
+   * 弹出Menu的属性
+   */
   MenuProps?: { [name: string]: any };
+  /**
+   * 值变更时的回调函数
+   */
   onChange?: (
     event: React.ChangeEvent<HTMLSelectElement> | React.MouseEvent<HTMLElement>,
     value?: string | string[],
   ) => void;
+  /**
+   * 弹窗出现时的回调函数
+   */
   onOpen?: (event: any) => void;
+  /**
+   * 弹窗消失时的回调函数
+   */
   onClose?: (event: any) => void;
+  /**
+   * 是否显示弹窗
+   */
   open?: boolean;
+  /**
+   * 是否只读
+   */
   readOnly?: boolean;
+  /**
+   * 不可用状态
+   */
   disabled?: boolean;
+  /**
+   * 值的渲染方式
+   */
   renderValue?: (value: string | string[]) => void;
+  /**
+   * 值
+   */
   value?: string | string[];
+  /**
+   * 下拉框类型
+   */
   variant?: 'standard' | 'outlined' | 'filled';
+  /**
+   * 最小宽度
+   */
   minWidth?: number;
+  /**
+   * 错误信息
+   */
   error?: string;
+  /**
+   * 密集模式
+   */
   dense?: boolean;
+  /**
+   * 辅助性文本
+   */
   helperText?: string;
+  /**
+   * 自定义class名称
+   */
   className?: string;
+  /**
+   * 是否必填
+   */
   required?: boolean;
+  /**
+   * 是否允许清空
+   */
   allowClear?: boolean;
 }
 
@@ -69,7 +137,6 @@ const Select = React.forwardRef<HTMLElement, Props>(function Select(
     input,
     inputProps,
     label,
-    labelWidth = 0,
     MenuProps,
     multiple = false,
     onClose: onCloseProp,
@@ -144,7 +211,6 @@ const Select = React.forwardRef<HTMLElement, Props>(function Select(
         <MdArrowDropDown />
       </InputAdornment>
     ),
-    labelWidth,
     value,
     disabled,
     readOnly,
