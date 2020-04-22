333912e454e247dba741836bc372d1a00f4547ad
tianyanqiu <745916729@qq.com>
2020-04-22 15:15:15 +0800

    chore: 导出FormControlProps声明

--- src/FormControl/index.ts
+++ src/FormControl/index.ts
@@ -1,5 +1,6 @@
 import FormControl from './FormControl';
+import type FormControlProps from './FormControlProps';
 
 export { default as useFormControlContext } from './useFormControlContext';
-
+export type { FormControlProps };
 export default FormControl;
