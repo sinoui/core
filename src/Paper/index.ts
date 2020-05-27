7416033bae70c05af8c8764f88103997b4f6f6ed
1690133293@qq.com <1690133293@qq.com>
2020-05-27 10:10:11 +0800

    chore: 发布 1.0.0-beta.31 版本

--- src/Paper/index.ts
+++ src/Paper/index.ts
@@ -1,6 +1,5 @@
-import Paper, { roundCss } from './Paper';
+import Paper from './Paper';
 import type { PaperProps } from './Paper';
 
 export type { PaperProps };
 export default Paper;
-export { roundCss };
