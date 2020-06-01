eb80ea9d2157aeb4b448f21e7634b178a8f3399d
1690133293@qq.com <1690133293@qq.com>
2020-06-01 17:14:42 +0800

    chore: Link 组件调整

---
+++ docs/docs/components/Link-demos/LinkDemo.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+import Typography from '@sinoui/core/Typography';
+import Link from '@sinoui/core/Link';
+
+function LinkDemo() {
+  const TypographyWrapper = styled(Typography)`
+    > a:not(:first-child) {
+      margin-left: 10px;
+    }
+  `;
+
+  return (
+    <TypographyWrapper>
+      <Link href="/docs/components/bottomSheet">BottomSheet</Link>
+      <Link href="/docs/components/paper">Paper</Link>
+      <Link href="/docs/components/bottomNavigation">bottomNavigation</Link>
+    </TypographyWrapper>
+  );
+}
+
+export default LinkDemo;
--- stories/Link.stories.tsx
+++ stories/Link.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import { defaultTheme } from '@sinoui/theme';
 import Link from '@sinoui/core/Link';
 import Typography from '@sinoui/core/Typography';
@@ -8,15 +8,21 @@ export default {
   title: 'Link',
 };
 
+const TypographyWrapper = styled(Typography)`
+  > a:not(:first-child) {
+    margin-left: 10px;
+  }
+`;
+
 export const 基本使用 = () => (
   <ThemeProvider theme={defaultTheme}>
-    <Typography>
+    <TypographyWrapper>
       <Link href="/?path=/story/typography--基本组件使用">Link</Link>
       <Link href="/?path=/story/typography--设置属性">Link-1</Link>
       <Link href="/?path=/story/typography--设置颜色" color="primary">
         Link-2
       </Link>
-    </Typography>
+    </TypographyWrapper>
   </ThemeProvider>
 );
 
@@ -36,11 +42,33 @@ export const 指定颜色 = () => (
       <Link href="/?path=/story/typography--设置颜色" color="primary">
         color=primary
       </Link>
+      <br />
+      <Link href="/?path=/story/typography--设置颜色" color="secondary">
+        color=secondary
+      </Link>
+      <br />
+      <Link href="/?path=/story/typography--设置颜色" color="textPrimary">
+        color=textPrimary
+      </Link>
+      <br />
+      <Link href="/?path=/story/typography--设置颜色" color="textSecondary">
+        color=textSecondary
+      </Link>
+      <br />
+      <Link href="/?path=/story/typography--设置颜色" color="error">
+        color=error
+      </Link>
+      <br />
       <Link href="/?path=/story/typography--设置颜色" color="warning">
         color=warning
       </Link>
+      <br />
       <Link href="/?path=/story/typography--设置颜色" color="success">
         color=success
+      </Link>
+      <br />
+      <Link href="/?path=/story/typography--设置颜色" color="info">
+        color=info
       </Link>
     </Typography>
   </ThemeProvider>
