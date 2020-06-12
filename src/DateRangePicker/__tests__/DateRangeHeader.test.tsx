09281cb0f7b3ed834c997f57b38a1169075f69c1
tianyanqiu <745916729@qq.com>
2020-06-12 14:14:37 +0800

    chore(DateRangePicker): 补充跨年切换上、下月的测试场景

--- src/DateRangePicker/__tests__/DateRangeHeader.test.tsx
+++ src/DateRangePicker/__tests__/DateRangeHeader.test.tsx
@@ -49,6 +49,29 @@ it('上个月', () => {
   expect(onChange).toBeCalledWith(2020, 4);
 });
 
+it('跨年切换上个月', () => {
+  const onChange = jest.fn();
+  const { getByTestId } = render(
+    <ThemeProvider theme={defaultTheme}>
+      <DateRangeHeader
+        startYear={2020}
+        startMonth={0}
+        data-testid="date-range-header"
+        onChange={onChange}
+      />
+    </ThemeProvider>,
+  );
+
+  const header = getByTestId('date-range-header');
+  const prevButton = header.querySelector(
+    '.sinoui-date-range-picker-header__prev-month-icon',
+  );
+
+  fireEvent.click(prevButton!);
+
+  expect(onChange).toBeCalledWith(2019, 11);
+});
+
 it('下个月', () => {
   const onChange = jest.fn();
   const { getByTestId } = render(
@@ -70,4 +93,27 @@ it('下个月', () => {
   fireEvent.click(nextButton!);
 
   expect(onChange).toBeCalledWith(2020, 6);
+});
+
+it('跨年切换下个月', () => {
+  const onChange = jest.fn();
+  const { getByTestId } = render(
+    <ThemeProvider theme={defaultTheme}>
+      <DateRangeHeader
+        startYear={2020}
+        startMonth={11}
+        data-testid="date-range-header"
+        onChange={onChange}
+      />
+    </ThemeProvider>,
+  );
+
+  const header = getByTestId('date-range-header');
+  const nextButton = header.querySelector(
+    '.sinoui-date-range-picker-header__next-month-icon',
+  );
+
+  fireEvent.click(nextButton!);
+
+  expect(onChange).toBeCalledWith(2021, 0);
 });
