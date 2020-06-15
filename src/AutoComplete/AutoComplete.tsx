bc9ea06d06895606b2bb0bc475aae8c7530f0034
tianyanqiu <745916729@qq.com>
2020-06-15 15:41:18 +0800

    fix(AutoComplete): 修复只读和不可用时弹窗指示器状态不正确的bug

--- src/AutoComplete/AutoComplete.tsx
+++ src/AutoComplete/AutoComplete.tsx
@@ -644,6 +644,7 @@ export default function AutoComplete(props: Props) {
       className="sinoui-auto-complete__popup-indicator"
       color={getPopupIndicatorColor()}
       onClick={handlePopupIndicatorClick}
+      disabled={disabled || readOnly}
     >
       {popupIcon}
     </PopupIndicatorWrapper>
