8a429fc2a4e630cb1303c81264aa267ff944750c
duanyaqin <361711931@qq.com>
2020-06-24 13:33:03 +0800

    chore: BottomSheet使用Slide动画

--- src/BottomSheet/BottomSheet.tsx
+++ src/BottomSheet/BottomSheet.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
 import classNames from 'classnames';
 import Modal from '@sinoui/core/Modal';
-import { CSSTransition } from 'react-transition-group';
+import Slide from '@sinoui/core/Slide';
 
 const ModalWrapper = styled(Modal)`
   display: flex;
   align-items: center;
-`;
-
-const ModalBody = styled.div`
-  height: 100%;
-  width: 100%;
-  display: flex;
 `;
 
 const BottomSheetWrapper = styled.div`
@@ -23,45 +17,7 @@ const BottomSheetWrapper = styled.div`
   align-self: flex-end;
   transform: translate3d(0, 0, 0);
   overflow-y: auto;
-`;
-
-const timeout = 300;
-
-const GlobalStyle = createGlobalStyle`
-.sinoui-bottom-sheet__translate-appear {
-  opacity: 0;
-  transform: translate3d(0, 100%, 0);
-}
-
-.sinoui-bottom-sheet__translate-appear-active {
-  opacity: 1;
-  transform: translate3d(0, 0, 0);
-  transition: opacity ${timeout}ms, transform ${timeout}ms, height ${timeout}ms;
-}
-
-.sinoui-bottom-sheet__translate-enter {
-  opacity: 0;
-  transform: translate3d(0, 100%, 0); 
-
-}
-
-.sinoui-bottom-sheet__translate-enter-active {
-  opacity: 1;
-  transform: translate3d(0, 0, 0);
-  transition: opacity ${timeout}ms, transform ${timeout}ms, height ${timeout}ms;
-}
-
-.sinoui-bottom-sheet__translate-exit { 
-  opacity: 1;
-  transform: translate3d(0, 0, 0);
- ;
-}
-
-.sinoui-bottom-sheet__translate-exit-active { 
-  opacity: 0;
-  transform: translate3d(0, 100%, 0);
-  transition: opacity ${timeout}ms, transform ${timeout}ms, height ${timeout}ms
-}
+  outline: none;
 `;
 export interface BottomSheetProps {
   /**
@@ -85,10 +41,6 @@ export interface BottomSheetProps {
    */
   children?: React.ReactNode;
   /**
-   * 动画延长时间
-   */
-  transitionDuration?: number;
-  /**
    * 过渡结束监听器
    */
   addEndListener?: any;
@@ -109,38 +61,32 @@ const BottomSheet = React.forwardRef(
       onBackdropClick,
       className,
       children,
-      transitionDuration = 300,
       addEndListener,
       ...rest
     } = props;
     return (
-      <ModalWrapper
-        {...rest}
-        open={!!open}
-        backdropClick={backdropClick}
-        onBackdropClick={onBackdropClick}
-        ref={ref}
-      >
-        <ModalBody ref={ref}>
-          <CSSTransition
-            classNames="sinoui-bottom-sheet__translate"
-            timeout={transitionDuration}
+      <>
+        <ModalWrapper
+          {...rest}
+          open={!!open}
+          backdropClick={backdropClick}
+          onBackdropClick={onBackdropClick}
+          ref={ref}
+        >
+          <Slide
             in={open}
             appear
-            unmountOnExit
+            direction="up"
             addEndListener={addEndListener}
           >
-            <>
-              <BottomSheetWrapper
-                className={classNames('sinoui-bottom-sheet', className)}
-              >
-                {children}
-              </BottomSheetWrapper>
-              <GlobalStyle />
-            </>
-          </CSSTransition>
-        </ModalBody>
-      </ModalWrapper>
+            <BottomSheetWrapper
+              className={classNames('sinoui-bottom-sheet', className)}
+            >
+              {children}
+            </BottomSheetWrapper>
+          </Slide>
+        </ModalWrapper>
+      </>
     );
   },
 );
