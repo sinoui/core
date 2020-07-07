f09a9b745f3588ea072027a9d981e4060cb9432b
tianyanqiu <745916729@qq.com>
2020-07-07 23:11:55 +0800

    chore(DateRangePicker): 调整顶部样式

--- src/DateRangePicker/MobileDateRangeView/MobileDateRangeViewToolBar.tsx
+++ src/DateRangePicker/MobileDateRangeView/MobileDateRangeViewToolBar.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import Caption from '@sinoui/core/Caption';
 import H5 from '@sinoui/core/H5';
+import IconButton from '@sinoui/core/IconButton';
+import Close from '@sinoui/core/svg-icons/Close';
+import Button from '@sinoui/core/Button';
 
 interface Props {
   /**
@@ -19,17 +22,38 @@ interface Props {
 }
 
 const MobileDateRangeViewToolBarWrapper = styled.div`
-  height: 96px;
-  padding: 16px 24px;
+  height: 128px;
+  padding: 8px 16px 24px 12px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   background-color: ${({ theme }) => theme.palette.primary.main};
   color: ${({ theme }) => theme.palette.primary.contrastText};
+  box-sizing: border-box;
 
   & > * {
     color: currentColor;
   }
+
+  .sinoui-icon-button,
+  .sinoui-button {
+    color: ${({ theme }) => theme.palette.primary.contrastText};
+  }
+
+  .sinoui-typography--caption,
+  .sinoui-typography--h5 {
+    padding-left: 48px;
+  }
+
+  .sinoui-typography--h5 {
+    font-size: 1.2rem;
+  }
+`;
+
+const MobileDateRangeViewToolBarAction = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 /**
@@ -46,6 +70,12 @@ const MobileDateRangeViewToolBar = ({
       className="sinoui-date-range-view-toolbar"
       {...rest}
     >
+      <MobileDateRangeViewToolBarAction>
+        <IconButton dense>
+          <Close />
+        </IconButton>
+        <Button>保存</Button>
+      </MobileDateRangeViewToolBarAction>
       <Caption>{title}</Caption>
       <H5>
         {startDate
