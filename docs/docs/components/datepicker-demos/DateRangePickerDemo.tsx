efafebaacb6e82b7649b8e1532389aad32c60a8a
刘进行 <jackingstarliu@gmail.com>
2020-07-15 11:53:17 +0800

    doc(DatePicker): 调整DateRangePicker的示例

--- docs/docs/components/datepicker-demos/DateRangePickerDemo.tsx
+++ docs/docs/components/datepicker-demos/DateRangePickerDemo.tsx
@@ -4,17 +4,28 @@ import styled, { ThemeProvider } from 'styled-components';
 import { defaultTheme } from '@sinoui/theme';
 
 const StyledDateRangePicker = styled(DateRangePicker)`
-  width: 200px;
+  width: 140px;
+  margin: 16px 0;
 `;
 
 export default function DateRangePickerDemo(props: any) {
   const [value, setValue] = useState(['2020-06-16', '2020-07-28']);
   return (
     <ThemeProvider theme={defaultTheme}>
+      <StyledDateRangePicker {...props} value={value} onChange={setValue} />
+
       <StyledDateRangePicker
         {...props}
         value={value}
-        onChange={(_value) => setValue(_value)}
+        onChange={setValue}
+        variant="outlined"
+      />
+
+      <StyledDateRangePicker
+        {...props}
+        value={value}
+        onChange={setValue}
+        variant="filled"
       />
     </ThemeProvider>
   );
