d6642a939e3dcb986cea2d984d212e07279d8780
tianyanqiu <745916729@qq.com>
2021-03-16 19:07:12 +0800

    chore(docs): 添加Steps组件的API文档

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -82,6 +82,7 @@ module.exports = {
       'components-apis/bottomNavigation',
       'components-apis/tooltip',
       'components-apis/navigationRail',
+      'components-apis/steps',
     ],
   },
 };
