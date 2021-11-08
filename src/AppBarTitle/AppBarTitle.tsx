39b22b6fb194dfddb11e9f2faad206a950998673
刘进行 <jackingstarliu@gmail.com>
2021-11-08 11:34:35 +0800

    fix(AppBarTitle): 修复 AppBarTitle 无 `sino-app-bar--title` class 的缺陷

--- src/AppBarTitle/AppBarTitle.tsx
+++ src/AppBarTitle/AppBarTitle.tsx
@@ -3,12 +3,12 @@ import styled from 'styled-components';
 /**
  * 顶部应用栏标题
  */
-const AppBarTitle = styled.h1((props) => {
-  return {
-    ...props.theme.typography.h6,
-    margin: 0,
-    overflow: 'hidden',
-  };
-});
+const AppBarTitle = styled.h1.attrs(() => ({
+  className: 'sinoui-app-bar--title',
+}))((props) => ({
+  ...props.theme.typography.h6,
+  margin: 0,
+  overflow: 'hidden',
+}));
 
 export default AppBarTitle;
