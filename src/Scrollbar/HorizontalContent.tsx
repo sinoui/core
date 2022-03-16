92a7f25e28c115a9a4f0ad1c5d15c93e318b5264
tianyanqiu <745916729@qq.com>
2022-03-16 14:42:50 +0800

    chore(Scrollbar): 添加改变内容尺寸的示例

--- src/Scrollbar/HorizontalContent.tsx
+++ src/Scrollbar/HorizontalContent.tsx
@@ -2,12 +2,11 @@ import styled from 'styled-components';
 
 const HorizontalContent = styled.div`
   position: relative;
-  display: block;
+  display: none;
   cursor: pointer;
   border-radius: inherit;
   background-color: rgba(0, 0, 0, 0.2);
   height: 100%;
-  width: 24px;
 `;
 
 export default HorizontalContent;
--- src/Scrollbar/VerticalContent.tsx
+++ src/Scrollbar/VerticalContent.tsx
@@ -2,12 +2,11 @@ import styled from 'styled-components';
 
 const VerticalContent = styled.div`
   position: relative;
-  display: block;
+  display: none;
   width: 100%;
   cursor: pointer;
   border-radius: inherit;
   background-color: rgba(0, 0, 0, 0.2);
-  height: 48px;
 `;
 
 export default VerticalContent;
