99b75cebb22d489af393b2f0c5d832d3b32cf881
刘进行 <jackingstarliu@gmail.com>
2022-05-05 10:54:31 +0800

    typo

--- src/utils/useElementResize.ts
+++ src/utils/useElementResize.ts
@@ -17,6 +17,24 @@ function getErd() {
 }
 
 /**
+ * 创建采用 raf 延迟执行回调函数的新函数
+ *
+ * @param callback 回调函数
+ * @returns 返回 raf 延迟执行回调函数的新函数
+ */
+function createRafCallback<T extends Function>(
+  callback: T,
+): T & { cancel: () => void } {
+  let raf = -1;
+  const rafCallback = (...args: any) => {
+    window.cancelAnimationFrame(raf);
+    raf = window.requestAnimationFrame(() => callback(...args));
+  };
+  rafCallback.cancel = () => window.cancelAnimationFrame(raf);
+  return rafCallback as any;
+}
+
+/**
  * 监听元素的尺寸变化
  *
  * > 注意：回调函数 listen 已经做了 debounce 处理
@@ -25,14 +43,20 @@ function getErd() {
  *
  * @param elementRef 需要监听尺寸变化的元素引用
  * @param listen 事件回调函数，尺寸发生变化时调用
+ * @param useRaf 是否采用 raf 延迟执行回调函数。默认采用的是 debounce 延迟执行回调函数
  */
 export default function useElementResize(
   elementRef: React.RefObject<HTMLElement | null> | undefined,
   listen: () => void,
+  useRaf = false,
 ) {
   const listenRef = useRef(listen);
   listenRef.current = listen;
-  const [callback] = useState(() => debounce(() => listenRef.current(), 64));
+  const [callback] = useState(() =>
+    useRaf
+      ? createRafCallback(() => listenRef.current())
+      : debounce(() => listenRef.current(), 64),
+  );
 
   useEffect(() => {
     window.addEventListener('resize', callback, false);
